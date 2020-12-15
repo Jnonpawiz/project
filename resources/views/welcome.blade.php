57a96b9d495a304c85b02e89f4693a1c16adc78d
Jnonpawiz <57086320+Jnonpawiz@users.noreply.github.com>
2020-12-15 16:45:11 +0700

    Initail Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Laravel + Vue</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script type="text/javascript" src="js/app.js"></script>
+    </body> 
+</html>
